2bc2721a5f696f0c84de92380632ec04db8eab5e
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2024-03-27 23:04:01 +0000

    Created Token Obtain Pair API View - Login View

--- backend/userauths/views.py
+++ backend/userauths/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+from userauths.models import Profile, User
+from userauths.serializer import MyTokenObtainPairSerializer, RegisterSerializer
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
