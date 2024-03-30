2b250bcf394056aa2f83d3561ea0970815b53138
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2024-03-30 02:08:43 +0000

    Added Logout Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,10 @@ import Register from './views/auth/register';
 
 import Login from './views/auth/login'
 
+import Dashboard from './views/auth/dashboard';
+
+import Logout from './views/auth/logout';
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -13,6 +17,8 @@ function App() {
     <Routes>
       <Route path='/login' element={<Login />} />
       <Route path='/register' element={<Register />} />
+      <Route path='/logout' element={<Logout />} />
+      <Route path='/' element={<Dashboard />} />
     </Routes>
   </BrowserRouter>
   )
