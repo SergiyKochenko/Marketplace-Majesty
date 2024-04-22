4e8cebc37bb6a7ed25c50ff4ca0e90b80483c6e8
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2024-04-22 22:47:22 +0100

    Added Wishlist API View

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -40,4 +40,5 @@ urlpatterns = [
     # Customer Endpoints
     path('customer/orders/<user_id>/', customer_views.OrdersAPIView.as_view()),
     path('customer/order/<user_id>/<order_oid>/', customer_views.OrderDetailAPIView.as_view()),
+    path('customer/wishlist/<user_id>/', customer_views.WishlistAPIView.as_view()),
 ]
--- backend/customer/views.py
+++ backend/customer/views.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
-from store.serializers import  ProductSerializer, ReviewSerializer, CategorySerializer, CartSerializer, CartOrderSerializer, CartOrderItemSerializer, CouponSerializer, NotificationSerializer
+from store.serializers import  ProductSerializer, ReviewSerializer, CategorySerializer, CartSerializer, CartOrderSerializer, CartOrderItemSerializer, CouponSerializer, NotificationSerializer, WishlistSerializer
 from userauths.models import User
 from store.models import Cart, CartOrderItem, Notification, Product, Category, CartOrder, Gallery, ProductFaq, Review,  Specification, Coupon, Color, Size, Tax, Wishlist, Vendor
 from decimal import Decimal
@@ -39,3 +39,31 @@ class OrderDetailAPIView(generics.RetrieveAPIView):
 
         order = CartOrder.objects.get(buyer=user, payment_status="paid", oid=order_oid)
         return order
+    
+class WishlistAPIView(generics.ListCreateAPIView):
+    serializer_class = WishlistSerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        user_id = self.kwargs['user_id']
+
+        user = User.objects.get(id=user_id)
+        wishlists = Wishlist.objects.filter(user=user)
+        return wishlists
+    
+    def create(self, request):
+        payload = request.data
+
+        product_id = payload['product_id']
+        user_id = payload['user_id']
+
+        product = Product.objects.get(id=product_id)
+        user = User.objects.get(id=user_id)
+
+        wishlist = Wishlist.objects.filter(product=product, user=user)
+        if wishlist:
+            wishlist.delete()
+            return Response({"message":"Wishlist deleted successfully."}, status=status.HTTP_200_OK)
+        else:
+            Wishlist.objects.create(product=product, user=user)
+            return Response({"message":"Added to wishlist."}, status=status.HTTP_201_CREATED)
