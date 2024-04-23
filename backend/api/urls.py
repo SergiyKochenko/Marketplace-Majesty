ac5e27b4bdff98424046207d3ca48575c4ea8575
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2024-04-23 11:47:18 +0100

    Added Customer Notification API View & Customer Notification Seen API View

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -41,4 +41,6 @@ urlpatterns = [
     path('customer/orders/<user_id>/', customer_views.OrdersAPIView.as_view()),
     path('customer/order/<user_id>/<order_oid>/', customer_views.OrderDetailAPIView.as_view()),
     path('customer/wishlist/<user_id>/', customer_views.WishlistAPIView.as_view()),
+    path('customer/notification/<user_id>/', customer_views.CustomerNotification.as_view()),
+    path('customer/notification/<user_id>/<noti_id>/', customer_views.MarkCustomerNotificationAsSeen.as_view()),
 ]
