2cc5125413075b4ca1922b5e8ed76170fb933cd6
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2024-04-26 00:08:43 +0100

    Added Chart API View for Orders and Products

--- backend/vendor/views.py
+++ backend/vendor/views.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.db import models
+from django.db.models.functions import ExtractMonth
 
 
 from store.serializers import  ProductSerializer, ReviewSerializer, CategorySerializer, CartSerializer, CartOrderSerializer, CartOrderItemSerializer, CouponSerializer, NotificationSerializer, WishlistSerializer, SummarySerializer
@@ -12,7 +13,7 @@ from decimal import Decimal
 import stripe
 from vendor.models import Vendor
 
-
+from rest_framework.decorators import api_view
 from rest_framework import generics, status
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
@@ -43,3 +44,19 @@ class DashboardStatsAPIView(generics.ListAPIView):
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
+    
+@api_view(('GET',))
+def MonthlyOrderChartAPIView(request, vendor_id):
+    vendor = Vendor.objects.get(id=vendor_id)
+    orders = CartOrder.objects.filter(vendor=vendor, payment_status="paid")
+    orders_by_month = orders.annotate(month=ExtractMonth("date")).values(
+        "month").annotate(orders=models.Count("id")).order_by("month")
+    return Response(orders_by_month)
+
+@api_view(('GET',))
+def MonthlyProductsChartAPIView(request, vendor_id):
+    vendor = Vendor.objects.get(id=vendor_id)
+    products = Product.objects.filter(vendor=vendor)
+    products_by_month = products.annotate(month=ExtractMonth("date")).values(
+        "month").annotate(products=models.Count("id")).order_by("month")
+    return Response(products_by_month)
