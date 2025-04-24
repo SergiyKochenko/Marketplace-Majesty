426b9e196f12561053573e498fa4589e7cbbb747
Sergiy <sergiykochenko@gmail.com>
2025-04-24 03:06:26 +0100

    Update URLs

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,3 @@
+export const BASE_URL = 'https://marketplace-majesty-production.up.railway.app/api/v1/';
 export const API_BASE_URL = 'http://127.0.0.1:8000/api/v1/';
-export const SERVER_URL = 'http://127.0.0.1:8000/'
+export const SERVER_URL = 'https://marketplace-majesty-production.up.railway.app';
